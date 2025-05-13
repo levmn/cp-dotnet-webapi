27325a54083d598d1cf7b2aca558085e1c87e760
levmn <levimncontato@gmail.com>
2025-05-12 21:00:59 -0300

    atualizando projeto buildado e openapi

--- LivrosApi/Program.cs
+++ LivrosApi/Program.cs
@@ -1,3 +1,5 @@
+using LivrosBusiness;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,7 @@ builder.Services.AddOpenApi();
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddScoped<LivroService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- LivrosModel/LivroModel.cs
+++ LivrosModel/LivroModel.cs
@@ -2,8 +2,8 @@ namespace LivrosModel;
 public class LivroModel
 {
     public int Id { get; set; }
-    public string Titulo { get; set; };
-    public string Autor { get; set; };
-    public string Genero { get; set; };
+    public string Titulo { get; set; } = string.Empty;
+    public string Autor { get; set; } = string.Empty;
+    public string Genero { get; set; } = string.Empty;
     public int AnoPublicacao { get; set; }
 }
